f0b2d2eabe9cbc3b0c755dd2440e147260f12ad2
mgl-inv <mgl.invaincu@yandex.ua>
2022-01-28 01:42:12 +0300

    comments deleted

--- src/FlagsmithHOC.js
+++ src/FlagsmithHOC.js
@@ -3,45 +3,45 @@ import flagsmith from 'flagsmith'
 const environmentID = 'WMNrzXLFoWrDZmnrd5gKxY'
 
 function flagsmithHOC(WrappedComponent, featureName) {
-  // ...и возвращает другой компонент...
-  return class extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {}
-    }
-  
-    componentWillMount() {
-      const { handleFlags, handleFlagsError } = this
-      flagsmith.init({
-        environmentID,
-        onChange: handleFlags,
-        onError: handleFlagsError,
-        cacheFlags: true,
-        enableLogs: true,
-        defaultFlags: {
-          some_button: true,
-          font_size: 12
-        }
-      })
-      // flagsmith.startListening(2000)
-    }
+	return class extends Component {
+		constructor(props) {
+			super(props)
+			this.state = {}
+		}
 
-    render() {
-      const someFeature = flagsmith.hasFeature(featureName)
-      
-      return someFeature && (
-        <WrappedComponent data={this.state.data} {...this.props} />
-      )
-    }
+		componentWillMount() {
+			const { handleFlags, handleFlagsError } = this
+			flagsmith.init({
+				environmentID,
+				onChange: handleFlags,
+				onError: handleFlagsError,
+				cacheFlags: true,
+				enableLogs: true,
+				defaultFlags: {
+					some_button: true,
+					font_size: 12
+				}
+			})
+		}
 
-    handleFlags = (oldFlags, params) => {
-      this.setState({
-        ...params
-      })
-    }
-  
-    handleFlagsError = (data) => {}
-  };
+		render() {
+			const someFeature = flagsmith.hasFeature(featureName)
+
+			return (
+				someFeature && (
+					<WrappedComponent data={this.state.data} {...this.props} />
+				)
+			)
+		}
+
+		handleFlags = (oldFlags, params) => {
+			this.setState({
+				...params
+			})
+		}
+
+		handleFlagsError = (data) => {}
+	}
 }
 
 export default flagsmithHOC
